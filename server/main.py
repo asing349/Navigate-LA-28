3c7f0a5a776385ff44d1c041f20acfb8910bbd4a
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-11-07 15:34:31 -0800

    Completed Initial Setup

---
+++ server/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+secret_key = os.getenv("SECRET_KEY")
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
