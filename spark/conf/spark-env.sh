780d155480c527dff3ebbdb22aeccb1be3f95b0a
Mohammed Faizaan Muzawar <faizaan.mm@gmail.com>
2024-11-26 19:18:35 -0800

    fixed errors

--- spark/conf/spark-env.sh
+++ spark/conf/spark-env.sh
@@ -7,3 +7,4 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
