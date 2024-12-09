4d64332cd9a748574e0fe17730316615e666b060
Mohammed Faizaan Muzawar <faizaan.mm@gmail.com>
2024-12-09 12:18:55 -0800

    added nearest query

--- server/services/nerest_restrooms.py
+++ server/services/nerest_restrooms.py
@@ -1,10 +1,17 @@
 from pyspark.sql import SparkSession
 
 # Initialize Spark Session (shared across API calls)
-spark = SparkSession.builder \
-    .appName("NearestRestroomsFinder") \
-    .config("spark.hadoop.fs.defaultFS", "hdfs://hadoop:9000") \
+spark = (
+    SparkSession.builder.appName("NearestPlacesFinder")
+    .config("spark.driver.memory", "2g")
+    .config("spark.executor.memory", "2g")
+    .config("spark.hadoop.fs.defaultFS", "hdfs://hadoop:9000")
+    .config("spark.hadoop.fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem")
+    .config("spark.hadoop.fs.file.impl", "org.apache.hadoop.fs.LocalFileSystem")
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-client:3.3.1")
     .getOrCreate()
+)
+
 
 # Function to find nearest restrooms
 def find_nearest_restrooms(x, y, n):
